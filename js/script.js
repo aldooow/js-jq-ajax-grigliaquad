d2dc99a116a0dccf316861ca31028054830f456c
Aldo Cahuana <aldooow@icloud.com>
2020-06-25 15:54:32 +0200

    Work completed!!! (Maybe!)

--- js/script.js
+++ js/script.js
@@ -6,11 +6,15 @@ Il numero ottenuto appare al centro del quadrato
 */
 $(document).ready(
   function(){
+
+    // Creare una Griglia 6x6.
     for(var i = 0; i < 36; i++){
       var template = $('.template').find('.box').clone();
       $('.wrapper').append(template);
     }
 
+    // Ad ogni click parte una richiesta AJAX che prende un numero random
+    // da 1 a 9.
     $(".box").click(function(){
       var box = this;
       $.ajax(
@@ -19,19 +23,22 @@ $(document).ready(
           method: 'GET',
           success: function(data){
             var numeroBox = parseInt(data.response);
+            // Se è <= 5 il quadrato diventa GIALLO,
             if(numeroBox <= 5){
               $(box).text(numeroBox);
               $(box).addClass('yellow');
               $(box).removeClass('green');
-            } else {
+            }
+            // se è > di 5 il quadrato diventa VERDE.
+            else {
               $(box).text(numeroBox);
               $(box).addClass('green');
               $(box).removeClass('yellow');
             }
 
           },
-          error: function(){
-
+          error: function(richiesta, stato, errore){
+              alert('ERROR')
           }
         }
       );
